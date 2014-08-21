bfd36892a742e8d18dff9026f09f5c78939a16e0
dluco <dluco11@gmail.com>
2014-08-21 10:42:03 -0700

    Added arch-exit to repo

--- c/ideas/ternary.c
+++ c/ideas/ternary.c
@@ -1,8 +1,23 @@
+/* preprocessor directives */
 #include <stdio.h>
+#define MAX 10
+
+/* function prototype */
+void foo(void);
 
 int main()
 {
-	(1) ? printf("TRUE\n") : printf("FALSE\n");
+//	(1 == 2) ? printf("TRUE\n") : printf("FALSE\n");
+	foo();
 
 	return 0;
 }
+
+void foo(void)
+{
+	if (1 == MAX) {
+		printf("TRUE\n");
+	} else {
+		printf("FALSE\n");
+	}
+}	
