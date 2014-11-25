e3f6ab972039b04ebb436ece71a1764a93716d2e
dluco <dluco11@gmail.com>
2014-11-25 15:00:34 -0800

    Using builtin sort

--- python/sort/sort.py
+++ python/sort/sort.py
@@ -2,33 +2,21 @@
 
 import fileinput
 
-def sort_sequential(str_list):
-    temp = list(str_list)
-    #result = []
-
-    for i in temp:
-        for j in temp:
-            test = cmp(i, j)
-            if test > 0:
-                # String i comes after String j
-                # Update i to be equal to j
-                i = j
-        print i
-
-
 def main():
     lines = []
 
+    # Read in input, line by line
     for line in fileinput.input():
         # Remove trailing newlines
         line = line.rstrip('\n\r')
         lines.append(line)
+    
+    # Sort input
+    lines.sort()
 
-    # No more input - echo input
+    # Echo input
     for line in lines:
         print line
-
-    sort_sequential(lines)
 
 if __name__ == "__main__":
     main()
