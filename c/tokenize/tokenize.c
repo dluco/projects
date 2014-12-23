2d8817be13f64c049f0c7ea63921195086b2222f
dluco <dluco11@gmail.com>
2014-12-23 12:34:41 -0800

    Added documentation

--- c/tokenize/tokenize.c
+++ c/tokenize/tokenize.c
@@ -1,3 +1,9 @@
+/*
+ * String Tokenizing:
+ * 	the reentrant version of strtok, strtok_r, is used
+ * 	to be thread safe.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,16 +18,31 @@ struct queue {
 	struct queue_node *tail;
 };
 
+/*
+ * initialize queue
+ *
+ * Return: none
+ */
 static void queue_init(struct queue *q)
 {
 	q->head = q->tail = NULL;
 }
 
+/*
+ * test if queue is empty (zero elements)
+ *
+ * Return: non-zero if empty, 0 if non-empty
+ */
 static int queue_isempty(struct queue *q)
 {
 	return (q->head == NULL && q->tail == NULL);
 }
 
+/*
+ * get the length of queue
+ *
+ * Return: length of queue
+ */
 static int queue_length(struct queue *q)
 {
 	struct queue_node *temp;
@@ -32,15 +53,19 @@ static int queue_length(struct queue *q)
 	return len;
 }
 
-/* add data to rear of queue */
-static void queue_enqueue(struct queue *q, char *str)
+/*
+ * add data to rear of queue
+ *
+ * Return: 0 on success, -1 on error
+ */
+static int queue_enqueue(struct queue *q, char *str)
 {
 	struct queue_node *temp;
 
 	/* create new element */
 	temp = malloc(sizeof *temp);
 	if (!temp) {
-		return;
+		return -1;
 	}
 	temp->data = strdup(str);
 	temp->next = NULL;
@@ -53,9 +78,15 @@ static void queue_enqueue(struct queue *q, char *str)
 		q->tail->next = temp;
 		q->tail = temp;
 	}
+
+	return 0;
 }
 
-/* remove data from front of queue */
+/*
+ * remove data from front of queue
+ *
+ * Return: dynamically allocated string on success, NULL on error
+ */
 static char *queue_dequeue(struct queue *q)
 {
 	struct queue_node *temp;
@@ -82,6 +113,11 @@ static char *queue_dequeue(struct queue *q)
 	return str;
 }
 
+/*
+ * free a queue's data and delete all elements
+ *
+ * Return: none
+ * */
 static void queue_free(struct queue *q)
 {
 	struct queue_node *temp, *old;
@@ -96,14 +132,17 @@ static void queue_free(struct queue *q)
 	}
 }
 
-/* tokenize the string str on delims - returns null-terminated array of strings */
+/*
+ * tokenize string on delimiters
+ *
+ * Return: null-terminated array of strings on success, NULL on error
+ */
 char **tokenize(char *str, char *delims)
 {
-	char *token, *s, *p;
-	char *saveptr;
+	char *token, *s, *p, *saveptr;
 	char **result;
-	struct queue q;
 	int i;
+	struct queue q;
 
 	/* make copy of input string */
 	s = strdup(str);
@@ -122,7 +161,7 @@ char **tokenize(char *str, char *delims)
 
 	free(s);
 
-	/* length of queue is the number of splits */
+	/* length of queue is the number of tokens */
 	result = malloc(queue_length(&q) * sizeof *result + 1);
 	if (!result) {
 		/* error */
@@ -131,6 +170,7 @@ char **tokenize(char *str, char *delims)
 	}
 
 	for (i = 0; !queue_isempty(&q); i++) {
+		/* empty out queue into result array */
 		result[i] = queue_dequeue(&q);
 	}
 	result[i] = NULL;
